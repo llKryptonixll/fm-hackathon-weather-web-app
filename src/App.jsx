14ca8d42b91cc7a6a54664f642f9107579a1c3d9
Lucas Cerri <lucascerri3@outlook.de>
2025-09-13 21:56:20 +0200

    vaiables and project setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-preset-1 f font-Bricolage flex gap-600 bg-red-900">
+      <div>t</div>
+      <div>t2</div>
+    </div>
+  )
+}
+
+export default App
